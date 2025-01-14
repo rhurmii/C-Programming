7b969b8e6bc1cee372b20ab5542bf80fb72fa191
rhurmii <22235103308@cse.bubt.edu.bd>
2025-01-14 12:21:10 +0600

    Add files via upload

---
+++ condition_type1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+    int taka;
+    scanf ("%d",&taka);
+    if (taka=1000)
+    {
+        printf ("I Will Eat Pizza");
+    }
+    else
+    {
+        printf ("I Won't Eat Anything");
+    }
+    return 0;
+}
