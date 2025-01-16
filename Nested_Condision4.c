a2ea7fc7311574801531d0bff9ac6388b0db7855
rhurmii <22235103308@cse.bubt.edu.bd>
2025-01-16 21:01:39 +0600

    Add files via upload

---
+++ Nested_Condision4.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main ()
+{
+    int taka;
+    scanf ("%d",&taka);
+    if (taka>=1000)
+    {
+        printf ("I Will Eat Pizza\n");
+
+
+
+    if ( taka>=500)
+    {
+        printf ("I Will Eat Burger\n");
+    }
+    else
+    {
+        printf ("I Won't Eat Anything\n");
+    }
+    }
+    else
+    {
+        printf ("I Will Go Anywhere\n");
+    }
+    return 0;
+}
