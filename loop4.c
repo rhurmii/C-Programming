18bd0f0e7cd6b1c8303ccc214151db24a2d13ed6
rhurmii <22235103308@cse.bubt.edu.bd>
2025-02-03 22:25:16 +0600

    Add files via upload

---
+++ loop4.c
@@ -0,0 +1,15 @@
+//You will be given a positive integer number N, you need to print I love practice N times.
+In c programming
+#include<stdio.h>
+int main()
+{
+    int i,N;
+    printf ("Enter the Number: ");
+    scanf ("%d",&N);
+    for(i=0;i<=N;i++)
+    {
+    printf ("I Love Practice\n");
+    }
+    return 0;
+}
+
